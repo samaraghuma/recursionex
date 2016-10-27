ea1cca78d6b1f38b2720c0f736d0ddd910d30594
raghuma sama <raghuma sama>
2016-10-27 11:23:42 -0400

    first commit

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static int fact(int x)
+        {
+            if (x==1)
+            {
+                return 1;
+                return x * x - 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fact(5));
+        }
+    }
+}
